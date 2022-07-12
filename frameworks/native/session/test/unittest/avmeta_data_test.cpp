8ee3e5d76648e1628267c447eb4080b21be7dfc7
guo-chunting <guochunting@DESKTOP-F45P4CV.localdomain>
2022-07-12 11:46:13 +0800

    Modify the test case of av metadata
    Signed-off-by: guo-chunting <guochunting@DESKTOP-F45P4CV.localdomain>

--- frameworks/native/session/test/unittest/avmeta_data_test.cpp
+++ frameworks/native/session/test/unittest/avmeta_data_test.cpp
@@ -24,17 +24,17 @@ using namespace testing::ext;
 using namespace OHOS::AVSession;
 using namespace OHOS::Media;
 
+namespace {
+    constexpr int64_t DURATION = 40000;
+    constexpr int64_t DURATION_PLAYBACK_SCENE_LIVE = -1;
+    constexpr int64_t DURATION_PLAYBACK_SCENE_INVALID = -2;
+    constexpr double PUBLISHDATE = 886291200000;
+    constexpr double PUBLISHDATE_INVALID_DATE = -1;
+}
+
 static AVMetaData g_metaDataCloneTest;
 static AVMetaData g_metaData;
 static OHOS::Parcel g_parcel;
-constexpr int64_t DURATION = 40000;
-constexpr int64_t DURATION_PLAYBACK_SCENE_LIVE = -1;
-constexpr int64_t DURATION_PLAYBACK_SCENE_INVALID = -2;
-constexpr double PUBLISHDATE = 886291200000;
-constexpr double PUBLISHDATE_INVALID_DATE = -1;
-static char g_testSessionTag[] = "test";
-static char g_testBundleName[] = "test.ohos.avsession";
-static char g_testAbilityName[] = "test.ability";
 
 class AVMetaDataTest : public testing::Test {
 public:
@@ -42,8 +42,7 @@ public:
     static void TearDownTestCase();
     void SetUp() override;
     void TearDown() override;
-
-    std::shared_ptr<AVSession> avsession_ = nullptr;
+    std::shared_ptr<OHOS::Media::PixelMap> CreatePixelMap();
 };
 
 void AVMetaDataTest::SetUpTestCase()
@@ -56,6 +55,29 @@ void AVMetaDataTest::TearDownTestCase()
 
 void AVMetaDataTest::SetUp()
 {
+    g_metaData.Reset();
+    g_metaData.SetAssetId("123");
+    g_metaData.SetTitle("Black Humor");
+    g_metaData.SetArtist("zhoujielun");
+    g_metaData.SetAuthor("zhoujielun");
+    g_metaData.SetAlbum("Jay");
+    g_metaData.SetWriter("zhoujielun");
+    g_metaData.SetComposer("zhoujielun");
+    g_metaData.SetDuration(DURATION);
+    g_metaData.SetMediaImage(CreatePixelMap());
+    g_metaData.SetMediaImageUri("https://baidu.yinyue.com");
+    g_metaData.SetPublishDate(PUBLISHDATE);
+    g_metaData.SetSubTitle("fac");
+    g_metaData.SetDescription("for friends");
+    g_metaData.SetLyric("https://baidu.yinyue.com");
+}
+
+void AVMetaDataTest::TearDown()
+{   
+}
+
+std::shared_ptr<OHOS::Media::PixelMap> AVMetaDataTest::CreatePixelMap()
+{
     int32_t pixelMapWidth = 4;
     int32_t pixelMapHeight = 3;
     const std::shared_ptr<OHOS::Media::PixelMap>& pixelMap = std::make_shared<OHOS::Media::PixelMap>();
@@ -68,40 +90,13 @@ void AVMetaDataTest::SetUp()
     int32_t rowDataSize = pixelMapWidth;
     uint32_t bufferSize = rowDataSize * pixelMapHeight;
     if (bufferSize <= 0) {
-        return;
+        return pixelMap;
     }
 
     std::vector<std::uint8_t> buffer(bufferSize, 0x03);
     pixelMap->SetPixelsAddr(buffer.data(), nullptr, bufferSize, AllocatorType::CUSTOM_ALLOC, nullptr);
 
-    g_metaData.Reset();
-    g_metaData.SetAssetId("123");
-    g_metaData.SetTitle("Black Humor");
-    g_metaData.SetArtist("zhoujielun");
-    g_metaData.SetAuthor("zhoujielun");
-    g_metaData.SetAlbum("Jay");
-    g_metaData.SetWriter("zhoujielun");
-    g_metaData.SetComposer("zhoujielun");
-    g_metaData.SetDuration(DURATION);
-    g_metaData.SetMediaImage(pixelMap);
-    g_metaData.SetMediaImageUri("https://baidu.yinyue.com");
-    g_metaData.SetPublishDate(PUBLISHDATE);
-    g_metaData.SetSubTitle("fac");
-    g_metaData.SetDescription("for friends");
-    g_metaData.SetLyric("https://baidu.yinyue.com");
-
-    OHOS::AppExecFwk::ElementName elementName;
-    elementName.SetBundleName(g_testBundleName);
-    elementName.SetAbilityName(g_testAbilityName);
-    avsession_ = AVSessionManager::CreateSession(g_testSessionTag, AVSession::SESSION_TYPE_AUDIO, elementName);
-    EXPECT_EQ(avsession_->SetAVMetaData(g_metaData), AVSESSION_SUCCESS);
-}
-
-void AVMetaDataTest::TearDown()
-{
-    if (avsession_ != nullptr) {
-        ASSERT_EQ(AVSESSION_SUCCESS, avsession_->Destroy());
-    }
+    return pixelMap;
 }
 
 /**
@@ -112,25 +107,7 @@ void AVMetaDataTest::TearDown()
 */
 HWTEST_F(AVMetaDataTest, SetAVMetaData001, TestSize.Level1)
 {
-    SLOGE("SetAVMetaData001 Begin");
-    int32_t pixelMapWidth = 4;
-    int32_t pixelMapHeight = 3;
-    const std::shared_ptr<OHOS::Media::PixelMap>& pixelMap = std::make_shared<OHOS::Media::PixelMap>();
-    ImageInfo info;
-    info.size.width = pixelMapWidth;
-    info.size.height = pixelMapHeight;
-    info.pixelFormat = PixelFormat::RGB_888;
-    info.colorSpace = ColorSpace::SRGB;
-    pixelMap->SetImageInfo(info);
-    int32_t rowDataSize = pixelMapWidth;
-    uint32_t bufferSize = rowDataSize * pixelMapHeight;
-    if (bufferSize <= 0) {
-        return;
-    }
-
-    std::vector<std::uint8_t> buffer(bufferSize, 0x03);
-    pixelMap->SetPixelsAddr(buffer.data(), nullptr, bufferSize, AllocatorType::CUSTOM_ALLOC, nullptr);
-
+    SLOGI("SetAVMetaData001 Begin");
     AVMetaData metaData;
     metaData.Reset();
     metaData.SetAssetId("123");
@@ -141,14 +118,32 @@ HWTEST_F(AVMetaDataTest, SetAVMetaData001, TestSize.Level1)
     metaData.SetWriter("zhoujielun");
     metaData.SetComposer("zhoujielun");
     metaData.SetDuration(DURATION);
-    metaData.SetMediaImage(pixelMap);
+    metaData.SetMediaImage(CreatePixelMap());
     metaData.SetMediaImageUri("https://baidu.yinyue.com");
     metaData.SetPublishDate(PUBLISHDATE);
     metaData.SetSubTitle("fac");
     metaData.SetDescription("for friends");
     metaData.SetLyric("https://baidu.yinyue.com");
-    EXPECT_EQ(avsession_->SetAVMetaData(metaData), AVSESSION_SUCCESS);
-    SLOGE("SetAVMetaData001 End");
+  
+    EXPECT_EQ("123", metaData.GetAssetId());
+    EXPECT_EQ("Black Humor", metaData.GetTitle());
+    EXPECT_EQ("zhoujielun", metaData.GetArtist());
+    EXPECT_EQ("zhoujielun", metaData.GetAuthor());
+    EXPECT_EQ("Jay", metaData.GetAlbum());
+    EXPECT_EQ("zhoujielun", metaData.GetWriter());
+    EXPECT_EQ("zhoujielun", metaData.GetComposer());
+    EXPECT_EQ("https://baidu.yinyue.com", metaData.GetMediaImageUri());
+
+    EXPECT_EQ(DURATION, metaData.GetDuration());
+    EXPECT_EQ(PUBLISHDATE, metaData.GetPublishDate());
+
+    EXPECT_NE(nullptr,metaData.GetMediaImage());
+
+    EXPECT_EQ("fac", metaData.GetSubTitle());
+    EXPECT_EQ("for friends", metaData.GetDescription());
+    EXPECT_EQ("https://baidu.yinyue.com", metaData.GetLyric());
+
+    SLOGI("SetAVMetaData001 End");
 }
 
 /**
@@ -159,14 +154,13 @@ HWTEST_F(AVMetaDataTest, SetAVMetaData001, TestSize.Level1)
 */
 HWTEST_F(AVMetaDataTest, SetAVMetaData002, TestSize.Level1)
 {
-    SLOGE("SetAVMetaData002 Begin");
+    SLOGI("SetAVMetaData002 Begin");
     AVMetaData metaData;
     metaData.Reset();
     metaData.SetAssetId("123");
     metaData.SetDuration(DURATION_PLAYBACK_SCENE_LIVE);
-
-    EXPECT_EQ(avsession_->SetAVMetaData(metaData), AVSESSION_SUCCESS);
-    SLOGE("SetAVMetaData002 End");
+    EXPECT_EQ(DURATION_PLAYBACK_SCENE_LIVE, metaData.GetDuration());
+    SLOGI("SetAVMetaData002 End");
 }
 
 /**
@@ -177,14 +171,13 @@ HWTEST_F(AVMetaDataTest, SetAVMetaData002, TestSize.Level1)
 */
 HWTEST_F(AVMetaDataTest, SetAVMetaData003, TestSize.Level1)
 {
-    SLOGE("SetAVMetaData003 Begin");
+    SLOGI("SetAVMetaData003 Begin");
     AVMetaData metaData;
     metaData.Reset();
     metaData.SetAssetId("123");
     metaData.SetDuration(DURATION_PLAYBACK_SCENE_INVALID);
-
-    EXPECT_NE(avsession_->SetAVMetaData(metaData), AVSESSION_SUCCESS);
-    SLOGE("SetAVMetaData003 End");
+    EXPECT_EQ(DURATION_PLAYBACK_SCENE_INVALID, metaData.GetDuration());
+    SLOGI("SetAVMetaData003 End");
 }
 
 /**
@@ -195,14 +188,13 @@ HWTEST_F(AVMetaDataTest, SetAVMetaData003, TestSize.Level1)
 */
 HWTEST_F(AVMetaDataTest, SetAVMetaData004, TestSize.Level1)
 {
-    SLOGE("SetAVMetaData004 Begin");
+    SLOGI("SetAVMetaData004 Begin");
     AVMetaData metaData;
     metaData.Reset();
     metaData.SetAssetId("123");
     metaData.SetPublishDate(PUBLISHDATE_INVALID_DATE);
-
-    EXPECT_NE(avsession_->SetAVMetaData(metaData), AVSESSION_SUCCESS);
-    SLOGE("SetAVMetaData004 End");
+    EXPECT_EQ(PUBLISHDATE_INVALID_DATE, metaData.GetPublishDate());
+    SLOGI("SetAVMetaData004 End");
 }
 /**
 * @tc.name: GetAVMetaData001
@@ -212,15 +204,15 @@ HWTEST_F(AVMetaDataTest, SetAVMetaData004, TestSize.Level1)
 */
 HWTEST_F(AVMetaDataTest, GetAVMetaData001, TestSize.Level1)
 {
-    SLOGE("GetAVMetaData001 Begin");
+    SLOGI("GetAVMetaData001 Begin");
     AVMetaData metaData;
     metaData.Reset();
     metaData.CopyFrom(g_metaData);
     EXPECT_EQ(metaData.GetAssetId(), g_metaData.GetAssetId());
     std::string title1 = metaData.GetTitle();
-    SLOGE("title1 %{public}s", title1.c_str());
+    SLOGI("title1 %{public}s", title1.c_str());
     std::string title2 = g_metaData.GetTitle();
-    SLOGE("title2 %{public}s", title2.c_str());
+    SLOGI("title2 %{public}s", title2.c_str());
     EXPECT_EQ(title1, title2);
     EXPECT_EQ(metaData.GetTitle(), g_metaData.GetTitle());
     EXPECT_EQ(metaData.GetArtist(), g_metaData.GetArtist());
@@ -235,7 +227,7 @@ HWTEST_F(AVMetaDataTest, GetAVMetaData001, TestSize.Level1)
     EXPECT_EQ(metaData.GetSubTitle(), g_metaData.GetSubTitle());
     EXPECT_EQ(metaData.GetDescription(), g_metaData.GetDescription());
     EXPECT_EQ(metaData.GetLyric(), g_metaData.GetLyric());
-    SLOGE("GetAVMetaData001 End");
+    SLOGI("GetAVMetaData001 End");
 }
 
 /**
@@ -246,11 +238,11 @@ HWTEST_F(AVMetaDataTest, GetAVMetaData001, TestSize.Level1)
  */
 HWTEST_F(AVMetaDataTest, AVMetaDataMarshalling001, TestSize.Level1)
 {
-    SLOGI("AVMetaDataMarshalling001 end");
+    SLOGI("AVMetaDataMarshalling001 Begin");
     OHOS::Parcel& parcel = g_parcel;
     auto ret = g_metaData.Marshalling(parcel);
     EXPECT_EQ(ret, true);
-    SLOGI("AVMetaDataMarshalling001 end");
+    SLOGI("AVMetaDataMarshalling001 End");
 }
 
 /**
@@ -261,11 +253,11 @@ HWTEST_F(AVMetaDataTest, AVMetaDataMarshalling001, TestSize.Level1)
  */
 HWTEST_F(AVMetaDataTest, AVMetaDataUnmarshalling001, TestSize.Level1)
 {
-    SLOGI("AVMetaDataUnmarshalling001 begin");
+    SLOGI("AVMetaDataUnmarshalling001 Begin");
     OHOS::Parcel& parcel = g_parcel;
     auto unmarshallingPtr = g_metaData.Unmarshalling(parcel);
     EXPECT_NE(unmarshallingPtr, nullptr);
-    SLOGI("AVMetaDataUnmarshalling001 end");
+    SLOGI("AVMetaDataUnmarshalling001 End");
 }
 
 /**
@@ -276,12 +268,12 @@ HWTEST_F(AVMetaDataTest, AVMetaDataUnmarshalling001, TestSize.Level1)
  */
 HWTEST_F(AVMetaDataTest, AVMetaDataGetMask001, TestSize.Level1)
 {
-    SLOGI("AVMetaDataGetMask001 begin");
+    SLOGI("AVMetaDataGetMask001 Begin");
     AVMetaData metaData;
     metaData.Reset();
     metaData.SetAssetId("123");
     EXPECT_NE(metaData.GetMetaMask().to_string(), "");
-    SLOGI("AVMetaDataGetMask001 end");
+    SLOGI("AVMetaDataGetMask001 End");
 }
 
 /**
@@ -292,14 +284,18 @@ HWTEST_F(AVMetaDataTest, AVMetaDataGetMask001, TestSize.Level1)
  */
 HWTEST_F(AVMetaDataTest, AVMetaDataCopyDataFromMetaIn001, TestSize.Level1)
 {
-    SLOGI("AVMetaDataCopyDataFromMetaIn001 begin");
+    SLOGI("AVMetaDataCopyDataFromMetaIn001 Begin");
     AVMetaData metaData;
     g_metaDataCloneTest.SetAssetId("1118");
     g_metaDataCloneTest.SetWriter("Jay Chou");
     g_metaDataCloneTest.SetDuration(DURATION);
     auto ret = metaData.CopyFrom(g_metaDataCloneTest);
+
     EXPECT_EQ(ret, true);
-    SLOGI("AVMetaDataCopyDataFromMetaIn001 end");
+    EXPECT_EQ(metaData.GetAssetId(),g_metaDataCloneTest.GetAssetId());
+    EXPECT_EQ(metaData.GetWriter(),g_metaDataCloneTest.GetWriter());
+    EXPECT_EQ(metaData.GetDuration(),g_metaDataCloneTest.GetDuration());
+    SLOGI("AVMetaDataCopyDataFromMetaIn001 End");
 }
 
 /**
@@ -310,15 +306,18 @@ HWTEST_F(AVMetaDataTest, AVMetaDataCopyDataFromMetaIn001, TestSize.Level1)
  */
 HWTEST_F(AVMetaDataTest, AVMetaDataCopyDataByMask001, TestSize.Level1)
 {
-    SLOGI("AVMetaDataCopyDataByMask001 begin");
+    SLOGI("AVMetaDataCopyDataByMask001 Begin");
     AVMetaData metaOut;
     metaOut.SetAssetId("a");
     metaOut.SetWriter("b");
     metaOut.SetDuration(0);
     AVMetaData::MetaMaskType mask = metaOut.GetMetaMask();
-    g_metaDataCloneTest.CopyToByMask(mask, metaOut);
+
+    auto ret = g_metaDataCloneTest.CopyToByMask(mask, metaOut);
+    EXPECT_EQ(ret, true);
+
     EXPECT_EQ(metaOut.GetAssetId(), g_metaDataCloneTest.GetAssetId());
     EXPECT_EQ(metaOut.GetWriter(), g_metaDataCloneTest.GetWriter());
     EXPECT_EQ(metaOut.GetDuration(), g_metaDataCloneTest.GetDuration());
-    SLOGI("AVMetaDataCopyDataByMask001 end");
+    SLOGI("AVMetaDataCopyDataByMask001 End");
 }
