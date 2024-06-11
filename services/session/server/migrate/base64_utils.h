e92ac74516cde760e8509f88c2d47b12de7eb127
韩杰刚 <hanjiegang@huawei.com>
2024-06-11 16:00:53 +0000

    update services/session/server/migrate/base64_utils.h.
    Signed-off-by: 韩杰刚 <hanjiegang@huawei.com>

--- services/session/server/migrate/base64_utils.h
+++ services/session/server/migrate/base64_utils.h
@@ -23,57 +23,52 @@
 
 namespace OHOS::AVSession {
 class Base64Utils {
-    static const int NUMBER_ZERO = 0;
-    static const int NUMBER_ONE = 1;
-    static const int NUMBER_TWO = 2;
-    static const int NUMBER_THREE = 3;
-    static const int NUMBER_FOUR = 4;
-    static const int NUMBER_SIX = 6;
-
+    static constexpr int kNumberZero = 0;
+    static constexpr int kNumberOne = 1;
+    static constexpr int kNumberTwo = 2;
+    static constexpr int kNumberThree = 3;
+    static constexpr int kNumberFour = 4;
+    static constexpr int kNumberSix = 6;
 public:
-    static std::string Base64Encode(const std::vector<uint8_t> &data)
+    static std::string Base64Encode(const std::vector<uint8_t>& data)
     {
         std::string encoded;
         int i = 0;
-        uint8_t byte3[NUMBER_THREE] = {0};
-        uint8_t byte4[NUMBER_FOUR] = {0};
+        uint8_t byte3[kNumberThree] = {0};
+        uint8_t byte4[kNumberFour] = {0};
         for (uint8_t byte : data) {
             byte3[i++] = byte;
-            if (i == NUMBER_THREE) {
-                byte4[NUMBER_ZERO] = (byte3[NUMBER_ZERO] & 0xfc) >> NUMBER_TWO;
-                byte4[NUMBER_ONE] =
-                    ((byte3[NUMBER_ZERO] & 0x03) << NUMBER_FOUR) | ((byte3[NUMBER_ONE] & 0xf0) >> NUMBER_FOUR);
-                byte4[NUMBER_TWO] =
-                    ((byte3[NUMBER_ONE] & 0x0f) << NUMBER_TWO) | ((byte3[NUMBER_TWO] & 0xc0) >> NUMBER_SIX);
-                byte4[NUMBER_THREE] = byte3[NUMBER_TWO] & 0x3f;
-                for (i = 0; i < NUMBER_FOUR; i++) {
-                    encoded += base64_chars[byte4[i]];
+            if (i == kNumberThree) {
+                byte4[kNumberZero] = (byte3[kNumberZero] & 0xfc) >> kNumberTwo;
+                byte4[kNumberOne] = ((byte3[kNumberZero] & 0x03) << kNumberFour) | ((byte3[kNumberOne] & 0xf0) >> kNumberFour);
+                byte4[kNumberTwo] = ((byte3[kNumberOne] & 0x0f) << kNumberTwo) | ((byte3[kNumberTwo] & 0xc0) >> kNumberSix);
+                byte4[kNumberThree] = byte3[kNumberTwo] & 0x3f;
+                for (i = 0; i < kNumberFour; i++) {
+                    encoded += kBase64Chars[byte4[i]];
                 }
-                i = 0;
+                i = kNumberZero;
             }
         }
-        if (i != 0) {
-            for (int k = i; k < NUMBER_THREE; k++) {
-                byte3[k] = 0;
+        if (i != kNumberZero) {
+            for (int k = i; k < kNumberThree; k++) {
+                byte3[k] = kNumberZero;
             }
-            byte4[NUMBER_ZERO] = (byte3[NUMBER_ZERO] & 0xfc) >> NUMBER_TWO;
-            byte4[NUMBER_ONE] =
-                ((byte3[NUMBER_ZERO] & 0x03) << NUMBER_FOUR) | ((byte3[NUMBER_ONE] & 0xf0) >> NUMBER_FOUR);
-            byte4[NUMBER_TWO] = ((byte3[NUMBER_TWO] & 0x0f) << NUMBER_TWO) | ((byte3[NUMBER_TWO] & 0xc0) >> NUMBER_SIX);
-            for (int k = 0; k < i + 1; k++) {
-                encoded += base64_chars[byte4[k]];
+            byte4[kNumberZero] = (byte3[kNumberZero] & 0xfc) >> kNumberTwo;
+            byte4[kNumberOne] = ((byte3[kNumberZero] & 0x03) << kNumberFour) | ((byte3[kNumberOne] & 0xf0) >> kNumberFour);
+            byte4[kNumberTwo] = ((byte3[kNumberOne] & 0x0f) << kNumberTwo) | ((byte3[kNumberTwo] & 0xc0) >> kNumberSix);
+            for (int k = 0; k < i + kNumberOne; k++) {
+                encoded += kBase64Chars[byte4[k]];
             }
-            while (i++ < NUMBER_THREE) {
+            while (i++ < kNumberThree) {
                 encoded += '=';
             }
         }
         return encoded;
     }
-
 private:
-    static const std::string base64_chars;
+    static const std::string kBase64Chars;
 };
-const std::string Base64Utils::base64_chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+const std::string Base64Utils::kBase64Chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
                                               "abcdefghijklmnopqrstuvwxyz"
                                               "0123456789+/";
 } // namespace OHOS::AVSession
