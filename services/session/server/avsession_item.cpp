1404a1e10918c2223c9bfb60d4174da0a0627303
红袍小恶魔 <luyuchen3@huawei.com>
2025-01-26 19:14:50 +0800

    PC屏蔽serviceCallbackForCastNtf_
    Signed-off-by: 红袍小恶魔 <luyuchen3@huawei.com>

--- services/session/server/avsession_item.cpp
+++ services/session/server/avsession_item.cpp
@@ -641,13 +641,16 @@ sptr<IRemoteObject> AVSessionItem::GetAVCastControllerInner()
 
     sharedPtr->SetSessionTag(descriptor_.sessionTag_);
     sharedPtr->SetSessionId(descriptor_.sessionId_);
-    sharedPtr->SetSessionCallbackForCastCap([this](std::string sessionId, bool isPlaying, bool isMediaChange) {
-        if (serviceCallbackForCastNtf_) {
-            SLOGI("MediaCapsule CastCapsule for service isPlaying %{public}d, isMediaChange %{public}d",
-                isPlaying, isMediaChange);
-            serviceCallbackForCastNtf_(sessionId, isPlaying, isMediaChange);
-        }
-    });
+    if (descriptor_.sessionTag_ != "RemoteCast") {
+        sharedPtr->SetSessionCallbackForCastCap([this](std::string sessionId, bool isPlaying, bool isMediaChange) {
+            if (serviceCallbackForCastNtf_) {
+                SLOGI("MediaCapsule CastCapsule for service isPlaying %{public}d, isMediaChange %{public}d",
+                    isPlaying, isMediaChange);
+                serviceCallbackForCastNtf_(sessionId, isPlaying, isMediaChange);
+            }
+        });
+    }
+    
     InitializeCastCommands();
     return remoteObject;
 }
