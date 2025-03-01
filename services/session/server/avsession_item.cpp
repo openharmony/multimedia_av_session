4380706b111d48ccf411b993ecec1df5dcd5e0a9
l00913067 <liushichun@huawei.com>
2025-03-01 12:11:18 +0000

    avsessionItemLock_和sessionserviceLock死锁
    Signed-off-by: l00913067 <liushichun@huawei.com>

--- services/session/server/avsession_item.cpp
+++ services/session/server/avsession_item.cpp
@@ -1049,7 +1049,7 @@ int32_t AVSessionItem::DeleteSupportCastCommand(int32_t cmd)
             auto iter = std::remove(supportedCastCmds_.begin(), supportedCastCmds_.end(), cmd);
             supportedCastCmds_.erase(iter, supportedCastCmds_.end());
 
-            iter = std::remove(supportedCastCmds_.begin(), supportedCastCmds_.end(), 
+            iter = std::remove(supportedCastCmds_.begin(), supportedCastCmds_.end(),
                 AVCastControlCommand::CAST_CONTROL_CMD_FAST_FORWARD);
             supportedCastCmds_.erase(iter, supportedCastCmds_.end());
 
