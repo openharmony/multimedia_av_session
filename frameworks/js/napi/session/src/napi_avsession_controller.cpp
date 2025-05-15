94fb441a8801b591a15df38d2b9ca38bc25e54f5
张柏洋 <1668410927@qq.com>
2025-05-15 17:55:00 +0000

    update frameworks/js/napi/session/src/napi_avsession_controller.cpp.
    Signed-off-by: 张柏洋 <1668410927@qq.com>

--- frameworks/js/napi/session/src/napi_avsession_controller.cpp
+++ frameworks/js/napi/session/src/napi_avsession_controller.cpp
@@ -1124,7 +1124,7 @@ napi_value NapiAVSessionController::SendControlCommand(napi_env env, napi_callba
             } else if (ret == ERR_COMMAND_NOT_SUPPORT) {
                 context->errMessage = "SendControlCommand failed : native command not support";
             } else if (ret == ERR_COMMAND_SEND_EXCEED_MAX) {
-                context->errMessage = "SendControlCommand failed : native command send nums overload";
+                context->errMessage = "SendControlCommand failed : native command send nums overload,Controls the frequency of sending self-query and control commands";
             } else if (ret == ERR_NO_PERMISSION) {
                 context->errMessage = "SendControlCommand failed : native no permission";
             } else {
