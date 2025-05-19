e55c14708dd38f3271e207a8df4c45e8edb6907f
张柏洋 <1668410927@qq.com>
2025-05-19 11:29:56 +0000

    update frameworks/js/napi/session/src/napi_avcast_controller.cpp.
    Signed-off-by: 张柏洋 <1668410927@qq.com>

--- frameworks/js/napi/session/src/napi_avcast_controller.cpp
+++ frameworks/js/napi/session/src/napi_avcast_controller.cpp
@@ -787,9 +787,8 @@ napi_value NapiAVCastController::SetDisplaySurface(napi_env env, napi_callback_i
             } else if (ret == ERR_COMMAND_NOT_SUPPORT) {
                 context->errMessage = "SetDisplaySurface failed : native command not support";
             } else if (ret == ERR_COMMAND_SEND_EXCEED_MAX) {
-                std::string errone = "SetDisplaySurface failed : native command send nums overload,";
-                std::string errtwo = " controls the frequency of sending self-query and control commands";
-                context->errMessage = errone + errtwo;
+                context->errMessage = "SetDisplaySurface failed : native command send nums overload, \
+                    controls the frequency of sending self-query and control commands";
             } else if (ret == ERR_NO_PERMISSION) {
                 context->errMessage = "SetDisplaySurface failed : native no permission";
             } else {
@@ -1465,9 +1464,8 @@ std::string NapiAVCastController::GetSendControlCommandErrMsg(int32_t error)
             msg = "SendControlCommand failed : native command not support";
             break;
         case ERR_COMMAND_SEND_EXCEED_MAX:
-            std::string errone = "SendControlCommand failed : native command send nums overload,";
-            std::string errtwo = " controls the frequency of sending self-query and control commands";
-            msg = errone + errtwo;
+            msg = "SendControlCommand failed : native command send nums overload, \
+                controls the frequency of sending self-query and control commands";
             break;
         default:
             msg = "SendControlCommand failed : native server exception";
