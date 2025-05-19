97684868d6c88690d034deeed5fd6884848825ff
张柏洋 <1668410927@qq.com>
2025-05-19 11:33:27 +0000

    update frameworks/js/napi/session/src/napi_avsession_manager.cpp.
    Signed-off-by: 张柏洋 <1668410927@qq.com>

--- frameworks/js/napi/session/src/napi_avsession_manager.cpp
+++ frameworks/js/napi/session/src/napi_avsession_manager.cpp
@@ -935,9 +935,8 @@ napi_value NapiAVSessionManager::SendSystemControlCommand(napi_env env, napi_cal
             } else if (ret == ERR_PERMISSION_DENIED) {
                 context->errMessage = "SendSystemControlCommand failed : native send control command permission denied";
             } else if (ret == ERR_COMMAND_SEND_EXCEED_MAX) {
-                std::string errone = "SendSystemControlCommand failed : native send command overload,";
-                std::string errtwo = " controls the frequency of sending self-query and control commands";
-                context->errMessage = errone + errtwo;
+                context->errMessage = "SendSystemControlCommand failed : native send command overload, \
+                    controls the frequency of sending self-query and control commands";
             } else {
                 context->errMessage = "SendSystemControlCommand failed : native server exception";
             }
