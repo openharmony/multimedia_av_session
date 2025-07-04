6c99fa480cc26b10c741d729157f8552b6dedb33
王海龙 <wanghailong46@h-partners.com>
2025-07-04 09:20:04 +0800

    fixed 0704
    Signed-off-by: 王海龙 <wanghailong46@h-partners.com>

--- services/session/server/avsession_service.cpp
+++ services/session/server/avsession_service.cpp
@@ -2772,7 +2772,6 @@ void AVSessionService::OnClientDied(pid_t pid, pid_t uid)
         ReleaseCastSession();
     }
 #ifdef ENABLE_AVSESSION_SYSEVENT_CONTROL
-    AVRouter::GetInstance().IsStopCastDiscovery(pid);
     AVSessionSysEvent::GetInstance().ReportPlayingState(
         BundleStatusAdapter::GetInstance().GetBundleNameFromUid(uid));
 #endif
