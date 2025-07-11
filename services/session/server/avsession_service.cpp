a7cf8bed4fae5ce0915f709802fac15998e37772
Liusccc <liushichun@huawei.com>
2025-07-11 01:39:38 +0000

    codecheck
    Signed-off-by: Liusccc <liushichun@huawei.com>

--- services/session/server/avsession_service.cpp
+++ services/session/server/avsession_service.cpp
@@ -1339,8 +1339,8 @@ void AVSessionService::AddCastCapsuleServiceCallback(sptr<AVSessionItem>& sessio
                 session->GetBundleName().c_str(), isChange);
             UpdateTopSession(session);
             NotifySystemUI(nullptr, true, isPlaying, isChange);
-            bool ret = UpdateOrder(session);
-            CHECK_AND_RETURN_LOG(ret, "UpdateOrder fail");
+            bool updateOrderResult = UpdateOrder(session);
+            CHECK_AND_RETURN_LOG(updateOrderResult, "UpdateOrder fail");
         }
     });
 #endif // CASTPLUS_CAST_ENGINE_ENABLE
@@ -2905,8 +2905,8 @@ void AVSessionService::HandleOtherSessionPlaying(sptr<AVSessionItem>& session)
     CHECK_AND_RETURN_LOG(session != nullptr, "No other sessions are currently playing");
     UpdateTopSession(session);
     CHECK_AND_RETURN_LOG(topSession_ != nullptr, "topSession is nullptr, update topSession fail!");
-    bool ret = UpdateOrder(session);
-    CHECK_AND_RETURN_LOG(ret, "UpdateOrder fail");
+    bool updateOrderResult = UpdateOrder(session);
+    CHECK_AND_RETURN_LOG(updateOrderResult, "UpdateOrder fail");
     if ((session->GetSessionType() == "audio" || session->GetSessionType() == "video") &&
         session->GetUid() != ancoUid) {
         AVSessionService::NotifySystemUI(nullptr, true, IsCapsuleNeeded(), false);
