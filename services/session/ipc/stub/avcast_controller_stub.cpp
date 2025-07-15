4bdd66476918b7f3a06d03c07545843b6f6b2329
马广生 <maguangsheng1@h-partners.com>
2025-07-15 12:32:59 +0000

    fix format for arm64
    Signed-off-by: 马广生 <maguangsheng1@h-partners.com>

--- services/session/ipc/stub/avcast_controller_stub.cpp
+++ services/session/ipc/stub/avcast_controller_stub.cpp
@@ -224,7 +224,7 @@ int32_t AVCastControllerStub::HandleSetDisplaySurface(MessageParcel& data, Messa
     uint64_t uniqueId = pSurface->GetUniqueId();
 
     auto surfaceId = std::to_string(uniqueId);
-    SLOGI("Get surface id uint64_t: %{public}llu, get the string: %{public}s", uniqueId, surfaceId.c_str());
+    SLOGI("Get surface id uint64_t: %{public}" PRIu64 ", get the string: %{public}s", uniqueId, surfaceId.c_str());
     CHECK_AND_RETURN_RET_LOG(reply.WriteInt32(SetDisplaySurface(surfaceId)),
         AVSESSION_ERROR, "WriteInt32 result failed");
     return ERR_NONE;
