fa9deb7ac6deb177c4a37118290e3c69024d7300
zhang-minmin-jason <zhangminmin9@h-partners.com>
2026-03-17 18:07:10 +0800

    add userId for GetWantAgent function
    Signed-off-by: zhang-minmin-jason <zhangminmin9@h-partners.com>

--- services/session/server/avsession_item.cpp
+++ services/session/server/avsession_item.cpp
@@ -783,7 +783,7 @@ AbilityRuntime::WantAgent::WantAgent AVSessionItem::CreateWantAgentWithIndex(
         wants,
         nullptr
     );
-    auto launchAbility = AbilityRuntime::WantAgent::WantAgentHelper::GetWantAgent(wantAgentInfo, GetUid());
+    auto launchAbility = AbilityRuntime::WantAgent::WantAgentHelper::GetWantAgent(wantAgentInfo, GetUserId(), GetUid());
     CHECK_AND_RETURN_RET_LOG(launchAbility != nullptr, ability, "CreateWantAgentWithIndex failed");
     return *launchAbility;
 }
