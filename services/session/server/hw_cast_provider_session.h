dfece924ad05d5beb2692adba4d407fd295ea68c
weixin_43563670 <diyujia1@huawei.com>
2026-08-18 22:50:22 +0800

    update: 更新文件 hw_cast_provider_session.h
    Signed-off-by: weixin_43563670 <diyujia1@huawei.com>

--- services/session/server/hw_cast_provider_session.h
+++ services/session/server/hw_cast_provider_session.h
@@ -97,7 +97,7 @@ private:
     const std::string HIPLAY_CAST_MODE_CHANGE_RESULT = "HIPLAY_CAST_MODE_CHANGE_RESULT";
     const std::string HIPLAY_BYPASS_DATA = "HIPLAY_BYPASS_DATA";
     const std::string HIPLAY_QUERY_RESPONSE = "HIPLAY_QUERY_RESPONSE";
-    const std::string HIPLAY_DEVICE_INFO_DATA = "HIPLAY_DEVICE_INFO_DATA";
+    const std::string HIPLAY_DEVICE_INFO_RESULT = "HIPLAY_DEVICE_INFO_RESULT";
 };
 } // namespace OHOS::AVSession
 
