a46fef1dc0f0d51feb5f8301f0ae97f8a5cfc59d
weixin_43563670 <diyujia1@huawei.com>
2026-08-22 22:19:26 +0800

    update: 更新文件 hw_cast_provider_session.cpp
    Signed-off-by: weixin_43563670 <diyujia1@huawei.com>

--- services/session/server/hw_cast_provider_session.cpp
+++ services/session/server/hw_cast_provider_session.cpp
@@ -438,7 +438,7 @@ void HwCastProviderSession::OnHiplayEventRecv(const int32_t eventId, const std::
             AVRouter::GetInstance().OnSystemCommonEvent(HIPLAY_QUERY_RESPONSE, jsonParam);
             break;
         case HIPLAY_DEVICE_INFO:
-            AVRouter::GetInstance().OnSystemCommonEvent(HIPLAY_DEVICE_INFO_RESULT, jsonParam);
+            AVRouter::GetInstance().OnSystemCommonEvent(UPDATE_DEVICE_INFO, jsonParam);
             break;
         default:
             break;
