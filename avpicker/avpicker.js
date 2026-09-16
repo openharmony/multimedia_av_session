008344df38ec229e43ced43faa9f12884864ba9a
Zc19960817 <zhangchi218@h-partners.com>
2026-09-16 10:57:30 +0800

    修改设备列表DLNA设备图标
    Signed-off-by: Zc19960817 <zhangchi218@h-partners.com>

--- avpicker/avpicker.js
+++ avpicker/avpicker.js
@@ -18,6 +18,7 @@ if (!('finalizeConstruction' in ViewPU.prototype)) {
 }
 
 const TAG = 'avcastpicker_component ';
+const castAudioType = 4;
 const castPlusAudioType = 8;
 const t = 20;
 const HIGH_QUALITY_MAX_SCALE = 1.5;
@@ -1142,25 +1143,51 @@ export class AVCastPicker extends ViewPU {
         Column.pop();
     }
 
+    showDlnaIcon(item) {
+ 	    if (item.supportedProtocols === undefined) {
+ 	        return false;
+ 	    }
+ 	    return (item.supportedProtocols & castAudioType) !== 0;
+ 	}
+
     iconBuilder(b3, c3, d3 = null) {
         this.observeComponentCreation2((f3, g3) => {
             If.create();
             if (this.deviceInfoType === 'true') {
                 this.ifElseBranchUpdateFunction(0, () => {
-                    this.observeComponentCreation2((n3, o3) => {
-                        SymbolGlyph.create(!c3 ? { 'id': -1, 'type': -1, params: [b3.deviceIconName],
-                            'bundleName': '__harDefaultBundleName__', 'moduleName': '__harDefaultModuleName__' } :
-                            { 'id': -1, 'type': -1, params: [b3.selectedIconName],
-                            'bundleName': '__harDefaultBundleName__', 'moduleName': '__harDefaultModuleName__' });
-                        SymbolGlyph.fontSize('24vp');
-                        SymbolGlyph.fontColor((c3 && this.configurationColorMode !==
-                            ConfigurationColorMode.COLOR_MODE_DARK) ?
-                            [{ 'id': -1, 'type': 10001, params: ['sys.color.comp_background_emphasize'],
-                            'bundleName': '__harDefaultBundleName__', 'moduleName': '__harDefaultModuleName__' }] :
-                            [{ 'id': -1, 'type': 10001, params: ['sys.color.icon_primary'],
-                            'bundleName': '__harDefaultBundleName__', 'moduleName': '__harDefaultModuleName__' }]);
-                        SymbolGlyph.renderingStrategy(2);
-                    }, SymbolGlyph);
+                    if (this.showDlnaIcon(b3)) {
+ 	                    this.ifElseBranchUpdateFunction(0, () => {
+ 	                        this.observeComponentCreation2((n3, o3) => {
+ 	                            SymbolGlyph.create({ 'id': -1, 'type': -1, params: ['sys.symbol.DLNA'],
+ 	                                'bundleName': '__harDefaultBundleName__', 'moduleName': '__harDefaultModuleName__' });
+ 	                            SymbolGlyph.fontSize('24vp');
+ 	                            SymbolGlyph.fontColor((c3 && this.configurationColorMode !==
+ 	                                ConfigurationColorMode.COLOR_MODE_DARK) ?
+ 	                                [{ 'id': -1, 'type': 10001, params: ['sys.color.comp_background_emphasize'],
+ 	                                'bundleName': '__harDefaultBundleName__', 'moduleName': '__harDefaultModuleName__' }] :
+ 	                                [{ 'id': -1, 'type': 10001, params: ['sys.color.icon_primary'],
+ 	                                'bundleName': '__harDefaultBundleName__', 'moduleName': '__harDefaultModuleName__' }]);
+ 	                            SymbolGlyph.renderingStrategy(2);
+ 	                        }, SymbolGlyph);
+ 	                    });
+ 	                } else {
+ 	                    this.ifElseBranchUpdateFunction(1, () => {
+ 	                        this.observeComponentCreation2((n3, o3) => {
+ 	                            SymbolGlyph.create(!c3 ? { 'id': -1, 'type': -1, params: [b3.deviceIconName],
+ 	                                'bundleName': '__harDefaultBundleName__', 'moduleName': '__harDefaultModuleName__' } :
+ 	                                { 'id': -1, 'type': -1, params: [b3.selectedIconName],
+ 	                                'bundleName': '__harDefaultBundleName__', 'moduleName': '__harDefaultModuleName__' });
+ 	                            SymbolGlyph.fontSize('24vp');
+ 	                            SymbolGlyph.fontColor((c3 && this.configurationColorMode !==
+ 	                                ConfigurationColorMode.COLOR_MODE_DARK) ?
+ 	                                [{ 'id': -1, 'type': 10001, params: ['sys.color.comp_background_emphasize'],
+ 	                                'bundleName': '__harDefaultBundleName__', 'moduleName': '__harDefaultModuleName__' }] :
+ 	                                [{ 'id': -1, 'type': 10001, params: ['sys.color.icon_primary'],
+ 	                                'bundleName': '__harDefaultBundleName__', 'moduleName': '__harDefaultModuleName__' }]);
+ 	                            SymbolGlyph.renderingStrategy(2);
+ 	                        }, SymbolGlyph);
+ 	                    });
+ 	                }
                 });
             } else {
                 this.ifElseBranchUpdateFunction(1, () => {
